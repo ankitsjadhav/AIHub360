95c2699603fd2e275cea6bb6df7d780bf59192c9
ankitsjadhav <ankitsj7@gmail.com>
2024-09-24 08:34:25 +0530

    Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,21 +1,13 @@
+import { LandingNavbar } from "@/components/landing-navabar";
 import {Button} from "@/components/ui/button"
 import Link from "next/link";
 
 const LandingPage = () => {
     return (  
-        <div>
-            <div>
-                Landing Page(Unprotected)
-            </div>
-    
-            <Link href="/sign-in">
-                <Button>Login</Button>
-            </Link>
-            
-            <Link href="/sign-up">
-                <Button>Register</Button>
-            </Link>
-        </div>
+       <div className="h-full">
+        <LandingNavbar />
+
+       </div>
     );
 }
  
